131f3e400d9dd3dd990522d8aeb313ab5a79e467
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2014-07-03 13:26:52 +0200

    Version 0.0.2

--- raw2folders.py
+++ raw2folders.py
@@ -1,36 +1,46 @@
 #!/usr/bin/env python
 
+__author__ = 'Nikolaus Sonnenschein'
+__version__ = '0.0.2'
+
 import os
 import sys
 import shutil
 import glob
-from optparse import OptionParser
-
-usage = "usage: %prog [options]"
-parser = OptionParser(usage=usage)
+from argparse import ArgumentParser
 
 
-parser.add_option("-i", "--input", dest="input_directory", default=".",
-                  help="directory containing *.raw files (for example 5-3-2014_1000 uM Muconic aci_1001 + [73.04] - [44.65 - 45.35].raw)", metavar="PATH")
-parser.add_option("-o", "--output", dest="output_directory", default=".",
-                  help="where to put folders and files from processed *.raw files", metavar="PATH")
-parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+parser = ArgumentParser()
 
-(options, args) = parser.parse_args()
+parser.add_argument("input_directory", default=".",
+                  help="directory containing *.raw files (for example 5-3-2014_1000 uM Muconic aci_1001 + [73.04] - [44.65 - 45.35].raw)")
+parser.add_argument("output_directory", default=".",
+                  help="where to put folders and files from processed *.raw files in addition to CSV file containing all processed files")
+parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help='set for verbose output')
 
-if not os.path.exists(options.output_directory):
-    os.mkdir(options.output_directory)
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
 
-for path in glob.glob(os.path.join(options.input_directory, '*.raw')):
-    if options.verbose:
+args = parser.parse_args()
+if not os.path.exists(args.output_directory):
+    os.mkdir(args.output_directory)
+
+files_for_csv = set()
+for path in glob.glob(os.path.join(args.input_directory, '*.raw')):
+    if args.verbose:
         print "Processing", path
     basename = os.path.basename(path)
     (new_file, folder) = basename.split(' + ')
+    files_for_csv.add(new_file)
     folder = folder.replace('.raw', '')
-    folder = os.path.join(options.output_directory, folder)
+    folder = os.path.join(args.output_directory, folder)
     if not os.path.exists(folder):
         os.mkdir(folder)
     final_destination = os.path.join(folder, new_file+'.raw')
-    if options.verbose:
+    if args.verbose:
         print "Copying to", final_destination
     shutil.copy(path, final_destination)
+with open(os.path.join(args.output_directory,'processed_raw_files.csv'), 'w') as fhandle:
+    for file in files_for_csv:
+        fhandle.write(file + "\n")
